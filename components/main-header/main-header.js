7c4b33e556481422b581ccd775db0ecfb2052ae2
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2024-06-18 16:22:25 +0800

    output meals data

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 import logoImg from '@/assets/logo.png';
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
 export default function MainHeader() {
+  
     return (
         <>
             <MainHeaderBackground />
@@ -14,8 +17,12 @@ export default function MainHeader() {
                 </Link>
                 <nav className={classes.nav}>
                     <ul>
-                        <li><Link href={'/meals'}>Browse Meals</Link></li>
-                        <li><Link href={'/community'}>Foodies Community</Link></li>
+                        <li>
+                         <NavLink href={'/meals'}>Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href={'/community'}>Foodies Community</NavLink>
+                          </li>
                     </ul>
                 </nav>
             </header>
