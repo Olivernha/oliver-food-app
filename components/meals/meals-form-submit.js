f5768db9dc3ba19a787b4356b619af48c6bc0724
Nay_Htet_Aung <nayhtetaung201733@gmail.com>
2024-06-28 14:11:48 +0800

    server actions with useFormState

--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -1,5 +1,5 @@
 'use client';
-import { userFormStatus } from 'react-dom';
+import { useFormStatus } from 'react-dom';
 export default function MealsFormSubmit() {
     const {pending}= useFormStatus();
     return (
